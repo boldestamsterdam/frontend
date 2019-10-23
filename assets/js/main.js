5f509c7fbd88e9ae793e973290e2e2d5c2088d4d
Jasper Plender <jasper@worldwideweb.design>
2019-10-23 13:14:19 +0200

    v4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
+// header sticky na scroll
+
 var distance = $('.scrollnav').offset().top;
 
 $(window).scroll(function () {
+    // als window verder is gescrollt dan de afstand tussen top en de .scrollnav 
     if ($(this).scrollTop() >= distance) {
         $('nav.scrollnav').addClass('fixednav');
         $("main").css("margin-top", "53px");
@@ -10,14 +13,23 @@ $(window).scroll(function () {
     }
 });
 
+// bron: https://stackoverflow.com/questions/7543718/test-in-jquery-if-an-element-is-at-the-top-of-screen
 
-// https://stackoverflow.com/questions/7543718/test-in-jquery-if-an-element-is-at-the-top-of-screen
+// css animaties starten als er op zoeken icoon geklikt wordt.
 
-
-$("nav .zoeken").click(function(){
- $("form.zoeken").toggleClass("zoekenactive");
-     $("#header article").toggleClass("zoekenactive");
-     $("form input").focus();
-
-
+$("nav .zoeken").click(function () {
+    $("form.zoeken").toggleClass("zoekenactive");
+    $("#header article").toggleClass("zoekenactive");
+    // autofocus op input veld
+    $("form input").focus();
 });
+
+// zoek knop submit laten verschijnen als er getypt word
+
+$(".zoeken button").hide();
+  $(".zoeken input").keydown(function(){
+    $(".zoeken button").fadeIn();
+  });
+
+// bron: https://www.w3schools.com/jquery/event_keyup.asp
+
